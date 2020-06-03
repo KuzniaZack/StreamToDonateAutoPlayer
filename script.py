e47ca520a91b58ab29f49fbbe1ec4334412d2199
KuzniaZack <44276875+KuzniaZack@users.noreply.github.com>
2020-06-03 06:47:22 -0700

    Update script.py
    added comment explaining sleep timer in script

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ while True:
     for url in urls:
         Popen(['start', 'chrome' , url], shell=True)
 
+    #currently, the sleep timer is set at 10 seconds for proof of functionality, change this to 3000 to accurately play the duration of the video
     time.sleep(10)
 
     Popen('taskkill /F /IM chrome.exe', shell=True)
